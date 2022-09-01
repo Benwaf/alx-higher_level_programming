6d1e5b6ba52bf17de5646aeb73f89ba3da786a94
Eben Wafula <benwafriley@gmail.com>
2022-08-31 20:04:43 -0700

    rectifications

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,8 @@
-!/usr/bin/python3
+#!/usr/bin/python3
+# 3-common_elements.py
+# Eben Wafula
 
 
 def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
     return (set_1 & set_2)
